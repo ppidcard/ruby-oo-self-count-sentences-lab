be394c910fcc77b33dd1a639254179c5f6f70752
ppidcard <tvxqlikei@live.cn>
2021-07-13 18:04:47 +1000

    Done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,29 @@ require 'pry'
 class String
 
   def sentence?
-    
+    p self
+    self[self.length - 1] == '.'? true : false
   end
 
   def question?
-
+    p self
+    self[self.length - 1] == '?'? true : false
   end
 
   def exclamation?
-
+    self[self.length - 1] == '!'? true : false
   end
 
   def count_sentences
-
+    array = self.split(/[!?.]/)
+    new_array = []
+    array.each do |item| 
+      if item != ""
+        new_array << item
+      end
+    end
+    new_array.count
   end
 end
+
+p 'hello.'.sentence?
